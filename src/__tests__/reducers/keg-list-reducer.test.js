7a7e547aa4231e17edc63fb5136ca3adbee48f4c
Todd Walraven <cod4247@yahoo.com>
2020-05-01 14:47:23 -0700

    add and pass test for adding keg using reducer

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,7 +1,42 @@
 import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () => {
+
+  let action;
+  const kegData = {
+    name: "dead guy ale",
+    brand: "rogue Brewing",
+    price: "5.00",
+    alcoholContent: "5.7%",
+    count: "123",
+    id: 1
+  }
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
   });
+
+  test('should successfully add new keg data to masterKegList', () => {
+    const { name, brand, price, alcoholContent, count, id } = kegData;
+    action = {
+      type: 'ADD_KEG',
+      name: name,
+      brand: brand,
+      price: price,
+      alcoholContent: alcoholContent,
+      count: count,
+      id: id
+    };
+    expect(kegListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        brand: brand,
+        price: price,
+        alcoholContent: alcoholContent,
+        count: count,
+        id: id
+      }
+    })
+  });
+
 });
--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,3 +1,18 @@
 export default (state = {}, action) => {
-  return state;
+  switch (action.type) {
+  case 'ADD_KEG':
+    const { name, brand, price, alcoholContent, count, id } = action;
+    return Object.assign({}, state, {
+      [id]: {
+      name: name,
+      brand: brand,
+      price: price,
+      alcoholContent: alcoholContent,
+      count: count,
+      id: id
+      }
+    });
+  default:
+    return state;
+  }
 };
