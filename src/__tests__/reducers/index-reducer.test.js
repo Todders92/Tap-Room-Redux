2dc6a1123782063735d971029d765648c30fa196
Todd Walraven <cod4247@yahoo.com>
2020-05-07 21:36:09 -0700

    test functionality of root reducer

---
+++ src/__tests__/reducers/index-reducer.test.js
@@ -0,0 +1,47 @@
+import rootReducer from '../../reducers/index';
+import { createStore } from 'redux';
+import formVisibleReducer from '../../reducers/form-visible-reducer.js';
+import kegListReducer from '../../reducers/keg-list-reducer.js';
+
+let store = createStore(rootReducer);
+
+describe("rootReducer", () => {
+
+  test('Should return default state if no action type is recognized', () => {
+    expect(rootReducer({}, { type: null })).toEqual({
+      masterKegList: {},
+      formVisibleOnPage: false
+    });
+  });
+
+  test('Check that initial state of kegListReducer matches root reducer', () => {
+    expect(store.getState().masterKegList).toEqual(kegListReducer(undefined, { type: null }));
+  });
+
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
+  });
+
+  test('Check that initial state of kegListReducer matches root reducer', () => {
+    const action = {
+      type: 'ADD_BEER',
+      name: 'Beer 1',
+      brand: 'Beer 1 Brand',
+      price: "$5.00",
+      AlcoholContnet: "6.7%",
+      quantity: 124,
+      id: 1,
+    }
+    store.dispatch(action);
+    expect(store.getState().masterKegList).toEqual(kegListReducer(undefined, action));
+  });
+
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    const action = {
+      type: 'TOGGLE_FORM'
+    }
+    store.dispatch(action);
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
+  });
+
+});
